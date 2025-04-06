4837bcfb5275b66394c2a3b3e9b0c891d9765331
fruitman <fruitman@fruitmanui-MacBookAir.local>
2025-04-06 22:19:09 +0900

    commit4

---
+++ 정렬/EasyInsertionSort.py
@@ -0,0 +1,15 @@
+def find_ins_idx(r, v):
+    for i in range(0, len(r)):
+        if v < r[i]:
+            return i
+    return len(r)
+def ins_sort(a):
+    result = []
+    while a:
+        value = a.pop(0)
+        ins_indx = find_ins_idx(result, value)
+        result.insert(ins_indx, value)
+    return result
+
+d = [2, 4, 5, 1, 3]
+print(ins_sort(d))
